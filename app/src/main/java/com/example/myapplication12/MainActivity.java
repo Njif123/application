b9ce61f9e2fc7f14956a6a2e70ac34f18ab5fd56
Njif123 <chak.2014@yandex.ru>
2020-02-22 23:34:20 +0300

    add  project files

--- app/src/main/java/com/example/myapplication12/MainActivity.java
+++ app/src/main/java/com/example/myapplication12/MainActivity.java
@@ -9,62 +9,81 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> myArray;
     EditText et1;
     Button btn1;
     TextView tv1;
     String s, k;
-    ArrayList<String> list;
     private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = new ArrayList<>();
-        list.add("привЕт");
-        list.add("тОрты");
-        list.add("кАрта");
-        list.add("плОмба");
-        list.add("договОр");
-        list.add("звонИт");
-        list.add("экспЕрт");
-        list.add("бралАсь");
-        list.add("недУг");
+
         btn1 = (Button) findViewById(R.id.btn1);
         tv1 = (TextView) findViewById(R.id.tv1);
+        Arr();
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                srchArr(v);
+
+                searchWord(v);
             }
         });
 
     }
+    public void Arr(){
+        myArray = new ArrayList<>();
+        try{
+            File words = new File("/lop1v3.txt");
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(words), "UTF8"));
 
+                String str;
 
+                while ((str = in.readLine()) != null) {
+                    myArray.add(str);
+                }
 
-    public void srchArr(View view){
-        et1= findViewById(R.id.et1);
-        s=et1.getText().toString().toUpperCase();
-        Log.d(TAG, s);
-        for(int i = 0; i<list.size();i++){
-            k=list.get(i).toUpperCase();
-            Log.i(TAG, k);
-            if(s.equals(k)){
-                tv1.setText(list.get(i));
+                in.close();
+            }
+            catch (UnsupportedEncodingException e)
+            {
+                Log.e("myapp","Oshiba");
+            }
+            catch (IOException e)
+            {
+                Log.e("myapp", "2");
+            }
+            catch (Exception e)
+            {
+                Log.e("myapp","3");
+            }
+        }
+        public void searchWord(View view){
+        String s = et1.getText().toString();
+        for(int i = 0; i<myArray.size(); i++){
+            if(s.toUpperCase()==(String)(myArray.get(i)).toUpperCase()){
+                tv1.setText((String)(myArray.get(i)));
                 break;
             }
             else{
                 tv1.setText("Такого слова нет");
             }
         }
-
-    }
-}
-
+        }
+        }
 
 
 
