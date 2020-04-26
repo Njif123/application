a5cc11f0be95627553fd48d78b678c0401e5b904
Антон <chak.2014@yandex.ru>
2020-04-26 18:37:48 +0300

    add  project files

--- app/src/main/java/com/example/myapplication12/Proverk.java
+++ app/src/main/java/com/example/myapplication12/Proverk.java
@@ -1,5 +1,6 @@
 package com.example.myapplication12;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +23,7 @@ import java.util.Random;
 
 public class Proverk extends AppCompatActivity {
     textUtility TextUtility;
-
+    Integer b = 0;
 
 
     @Override
@@ -56,27 +59,54 @@ public class Proverk extends AppCompatActivity {
         glas.add('И');
         glas.add('Ю');
 
-        String result = test.get(random.nextInt(test.size() + 1)).toUpperCase();
-        SpannableString resultSpan = new SpannableString(result);
-        ClickableSpan clickableSpan = new ClickableSpan() {
+        final String result = test.get(random.nextInt(test.size() + 1)).toUpperCase();
+        SpannableStringBuilder resultSpan = new SpannableStringBuilder(result);
 
-            @Override
-            public void onClick(View textView) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Нажми ещё раз", Toast.LENGTH_SHORT);
-                toast.show();
-            }
+        for (int i = 0; i < result.toCharArray().length; i++) {
+            Character a = result.toCharArray()[i];
+            if (glas.contains(a)) {
+                SpannableString s = new SpannableString(a+"");
+                s.setSpan(getClickableSpan(i),
+                        0,
+                        s.length(),
+                        Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                resultSpan.replace(i, i + 1, s);
 
-        };
-        for (int j = 0; j<result.length(); j++) {
-            Character ch = result.toCharArray()[j];
-            if (glas.contains(ch)) {
-                resultSpan.setSpan(clickableSpan, j, j+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-                resultSpan.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)),
-                        j, j + 1,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             }
         }
+//        for (int j = 0; j<result.length(); j++) {
+//            Character ch = result.toCharArray()[j];
+//            if (glas.contains(ch)) {
+//                resultSpan.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)),
+//                        j, j + 1,
+//                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//                resultSpan.setSpan(getClickableSpan(j), j, j+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//            }
+//        }
         word.setText(resultSpan);
+    }
+
+
+
+    private ClickableSpan getClickableSpan(final int i) {
+        return new ClickableSpan() {
+            int c;
+
+            {
+                c = i;
+            }
+
+            @Override
+            public void onClick(@NonNull View widget) {
+                Toast.makeText(Proverk.this, "Нажми ещё раз", Toast.LENGTH_SHORT).show();
+                Log.d("TAG", "кликнулось");
+                synchronized (b) {
+
+                }
+                b = c;
+
+            }
+        };
     }
         @Override
         public void onBackPressed () {
